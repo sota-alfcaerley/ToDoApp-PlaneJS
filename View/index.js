6234d1043f66f671b625a6fe803e4ea1ea4629b5
Sota-k <violetdev9966@gmail.com>
2022-08-08 01:11:13 +0900

    Implement Add, Delete

--- View/index.js
+++ View/index.js
@@ -1,17 +1,52 @@
-// import ToDoItem from '../Models/todo'
+'use strict';
+var globalTodoItems;
 
 window.addEventListener('DOMContentLoaded', function () {
+    // ToDo初期値設定
     var todoItems = initializeToDoItems();
-    createToDoItemComponent(todoItems); 
-    item = todoItems;
+
+    // ToDo明細作成、イベントハンドラ定義
+    todoItems.filter(x => x.isDeleted === false).forEach(element => {
+        createToDoItemComponent(element); 
+    });
+
+    // モーダル制御用インスタンス
+    var addNewTaskModal = new bootstrap.Modal(document.getElementById('addNewTaskModal'), {
+        toggle: true
+    });
+
+    // プラスボタン押下時イベント
+    document.getElementById('addToDoButton').addEventListener('click', function(){
+        addNewTaskModal.toggle();
+    }, false);
+
+    // ToDo追加モーダル.追加ボタン押下時イベント
+    document.getElementById('addTaskButton').addEventListener('click', function(){
+        onClickAddTaskButton();
+        addNewTaskModal.toggle();
+    }, false)
+
+    // ToDo追加モーダル.下Xボタン押下時イベント
+    document.getElementById('bottomCloseButton').addEventListener('click', function(){
+        addNewTaskModal.toggle();
+    }, false)
+
+    // ToDo追加モーダル.上Xボタン押下時イベント
+    document.getElementById('topCloseButton').addEventListener('click', function(){
+        addNewTaskModal.toggle();
+    }, false);
+
+    globalTodoItems = todoItems;
+
 }, false);
 
+// アイテムクラス
 class ToDoItem {
-    constructor(id, title, priority, limit, isFinished ,isDeleted, createdAt, updatedAt){
+    constructor(id, title, priority, isFinished ,isDeleted, createdAt, updatedAt){
         this.id = id;
         this.title = title;
         this.priority = priority;
-        this.limit = limit;
+        // this.limit = limit;
         this.isFinished = isFinished;
         this.isDeleted = isDeleted;
         this.createdAt = createdAt;
@@ -24,9 +59,9 @@ function initializeToDoItems() {
     var todoItems = [
         new ToDoItem (
             1,
-            "Play Monster Hunter",
+            "Study javascript",
             "HIGH",
-            "2020/08/08",
+            // "2020/08/08",
             true,
             false,
             "2020/08/08",
@@ -36,7 +71,7 @@ function initializeToDoItems() {
             2,
             "Save Money",
             "HIGH",
-            "2020/08/08",
+            // "2020/08/08",
             false,
             false,
             "2020/08/08",
@@ -46,7 +81,7 @@ function initializeToDoItems() {
             3,
             "Remove Project",
             "HIGH",
-            "2020/08/08",
+            // "2020/08/08",
             false,
             false,
             "2020/08/08",
@@ -56,7 +91,7 @@ function initializeToDoItems() {
             4,
             "Study Vue",
             "HIGH",
-            "2020/08/08",
+            // "2020/08/08",
             true,
             false,
             "2020/08/08",
@@ -66,7 +101,7 @@ function initializeToDoItems() {
             5,
             "Read Invert",
             "HIGH",
-            "2020/08/08",
+            // "2020/08/08",
             false,
             false,
             "2020/08/08",
@@ -76,78 +111,116 @@ function initializeToDoItems() {
     return todoItems;
 };
 
-// UI部品作成
-function createToDoItemComponent(todoItems) {
-    todoItems.forEach(element => {
+// ToDo明細作成、イベントハンドラ定義
+function createToDoItemComponent(todoItem) {   
+        var list = document.getElementById('todo-item-list');
         var item = document.createElement('li');
         item.className = 'list-group-item';
+        list.appendChild(item);
+    
+
+        if(todoItem.isFinished === true){
+            item.style.backgroundColor = '#e6e6fa';
+        } else {
+            item.style.backgroundColor = '';
+        }
+
         var input_checkBox = document.createElement('input');
         input_checkBox.type = 'checkbox';
         input_checkBox.name = 'toggle-is-finished-checkbox';
         input_checkBox.className = 'form-check-input';
-        input_checkBox.checked = element.isFinished;
+        input_checkBox.checked = todoItem.isFinished;
         input_checkBox.addEventListener('change', function(){
-            onToggleCheckbox(element.id);
+            onToggleCheckbox(todoItem.id, input_checkBox.checked);
+            if(todoItem.isFinished === true){
+                item.style.backgroundColor = '#e6e6fa';
+            } else {
+                item.style.backgroundColor = '';
+            }
         });
         item.appendChild(input_checkBox);
 
         var id = document.createElement('a');
-        id.textContent = element.id;
+        id.textContent = todoItem.id;
         id.class = 'id';
         item.appendChild(id);
 
         var title = document.createElement('a');
-        title.textContent = element.title;
+        title.textContent = todoItem.title;
         title.class = 'title';
         item.appendChild(title);
 
         var priority = document.createElement('a');
-        priority.textContent = element.priority;
+        priority.textContent = todoItem.priority;
         priority.class = 'priority';
         item.appendChild(priority);
 
-        var limit = document.createElement('a');
-        limit.textContent = element.limit;
-        limit.class = 'limit';
-        item.appendChild(limit);
+        // var limit = document.createElement('a');
+        // limit.textContent = todoItem.limit;
+        // limit.class = 'limit';
+        // item.appendChild(limit);
 
-        var input_editButton = document.createElement('input');
-        input_editButton.type = 'button';
+        var input_editButton = document.createElement('button');
         input_editButton.name = 'edit-button';
         input_editButton.className = 'btn btn-primary';
         input_editButton.class = 'item-button';
         input_editButton.addEventListener('click', function(){
-            onClickEditIcon(element.id)
-        });
+            onClickEditButton(todoItem.id)
+        }, false);
         item.appendChild(input_editButton);
 
-        var input_deleteButton = document.createElement('input');
-        input_deleteButton.type = 'button';
+        var pencil_icon = document.createElement('i');
+        pencil_icon.className = 'bi bi-pen-fill';
+        input_editButton.appendChild(pencil_icon);
+
+        var input_deleteButton = document.createElement('button');
         input_deleteButton.name = 'delete-button';
         input_deleteButton.className = 'btn btn-primary';
         input_deleteButton.class='item-button';
         input_deleteButton.addEventListener('click', function(){
-            onclickDeleteButton(element.id)
-        });
+            onclickDeleteButton(todoItem.id, list, input_deleteButton)
+        }, false);
         item.appendChild(input_deleteButton);
 
-        var list = document.getElementById('todo-item-list');
-        list.appendChild(item);
-    });
+        var trush_icon = document.createElement('i');
+        trush_icon.className = 'bi bi-trash-fill';
+        input_deleteButton.appendChild(trush_icon);
+
 };
 
 function onClickPlusButton() {
 
 }
 
-function onToggleCheckbox(id, isFinished) {
-    console.log(id);
+// 
+function onClickAddTaskButton(){
+    var ids = globalTodoItems.map(function (p) {
+        return p.id;
+      });
+
+    var id = Math.max.apply(null, ids) + 1;
+    var title = document.getElementById('newTaskTextArea').value
+    var priority = document.getElementById('newTaskPrioritySelect').value;
+    var isFinished = false;
+    var isDeleted = false;
+    var createdAt = new Date().toDateString();
+    var updatedAt = new Date().toDateString();
+
+    var todoItem = new ToDoItem(id, title, priority, isFinished, isDeleted, createdAt, updatedAt)
+    createToDoItemComponent(todoItem);
+    globalTodoItems.push(todoItem);
 }
 
-function onClickEditIcon(id) {
+function onToggleCheckbox(id, checked) {
+    globalTodoItems.find(x => x.id === id).isFinished = checked;
+}
+
+function onClickEditButton(id) {
     console.log('click:',id);
 }
 
-function onclickDeleteButton(id) {
-    console.log('delete event:', id)
+function onclickDeleteButton(id, items, input_deleteButton) {
+    globalTodoItems.find(x => x.id === id).isDeleted = true;
+    var selectedTodo = input_deleteButton.closest('li');
+    items.removeChild(selectedTodo);
 }
